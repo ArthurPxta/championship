31b42a3202bb0b70e7e86d0edc7d2128c47efea3
Emanuel Bravo <emanuelbravo0706@gmail.com>
2023-02-06 20:55:11 -0300

    Adiciona funções novas e refatora
    Adiciona função contaCompetidores.
    Adiciona função achaCompetidor:
     -Ainda falta fazer funcionar, apenas foi adicionada a função
    
    Refatora função Cadastrar_Competidor:
     -Altera nome para cadastrarCompetidor, para formalizar de acordo com a regra camelCase
     - altera a geração do número de inscrição para ser sequencial de acordo com o número de participantes atuais, usando a funcão contaCompetidores
    
    faz a variavel FILE *arquivo global

--- Chanpionship.c
+++ Chanpionship.c
@@ -2,104 +2,151 @@
 #include <stdlib.h>
 #include <time.h>
 
+FILE *cadastro;
+
 typedef struct competidor
 {
-    char faixa[15], nome[50];
-    float peso;
-    int idade, inscricao;
+   char faixa[15], nome[50];
+   float peso;
+   int idade, inscricao;
 
-} competidor ;
+} competidor;
 
-int Cadastrar_Competidor(int n_competidores)
+int contaCompetidores()
 {
-    struct competidor competidor;
+   cadastro = fopen("cadastro.txt", "r");
+   // Testa se arquivo foi aberto corretamente
+   if (cadastro == NULL)
+   {
+      printf("Error opening file.\n");
+      printf("Function: getNumberOfStudents\n");
+      return 1;
+   }
 
-    FILE *cadastro = fopen("cadastro.txt", "a");
-            
-    printf("Informe o nome do competidor:");
-    scanf("%15[^\n]", competidor.nome);
-            
-    setbuf(stdin, NULL);
-            
-    printf("Informe a faixa do competidor:");
-    scanf("%50[^\n]", competidor.faixa);
-            
-    setbuf(stdin, NULL);
+   int nCompetidores = 1;
+   char temp;
 
-    printf("Informe o peso do competidor:");
-    scanf("%f", &competidor.peso);
-
-    //setbuf(stdin, NULL);
-            
-    printf("Informe a idade do competidor: ");
-    scanf("%d", &competidor.idade);
-
-    competidor.inscricao = rand()%99999;
-
-    fprintf(cadastro, "%d %s %s %.2f %d\n", competidor.inscricao, competidor.nome, competidor.faixa, competidor.peso, competidor.idade);
-            
-    fclose(cadastro);
-
-    n_competidores++;
-
-    return n_competidores;
+   while (fscanf(cadastro, "%c", &temp) != EOF)
+   {
+      if (temp == '\n')
+         nCompetidores++;
+   }
+   fclose(cadastro);
+   return nCompetidores;
 }
 
-int main ()
-{   
-    int opc, n_competidores = 0;
+void achaCompetidor()
+{
+   int inscricao;
+   printf("\nDigite o id do competidor que deseja: ");
+   scanf("%d", &inscricao);
+   printf("\nCompetidor de inscricao: %d", inscricao); // Fazer acentos funcionarem ??
+}
 
+// Alteração no nome da função: nome atualizado para seguir o padrão camelCase, convenção para nome de funções e variáveis
+// int Cadastrar_Competidor(int n_competidores)
+int cadastrarCompetidor(int nCompetidores)
+{
+   struct competidor competidor;
 
-    competidor competidor;
+   cadastro = fopen("cadastro.txt", "a");
+   // Testa se arquivo foi aberto corretamente
+   if (cadastro == NULL)
+   {
+      printf("Error opening file.\n");
+      printf("Function: getNumberOfStudents\n");
+      return 1;
+   }
 
-    while (1)
-    {
-        printf("\n------- MENU -------- \n");
-        printf("1) CADASTRAR COMPETIDOR \n");
-        printf("2) REMOVER COMPETIDOR \n");
-        printf("3) ALTERAR DADOS DO COMPETIDOR \n");
-        printf("4) COMECAR CAMPEONATO \n");
-        printf("5) SAIR \n");
+   printf("Informe o nome do competidor:");
+   scanf("%15[^\n]", competidor.nome);
 
-        printf("\n Digite a Opcao: ");
-        scanf("%d", &opc);
+   setbuf(stdin, NULL);
 
-        setbuf(stdin, NULL);
+   printf("Informe a faixa do competidor:");
+   scanf("%50[^\n]", competidor.faixa);
 
-        switch (opc)
-        {
-        case 1 :
+   setbuf(stdin, NULL);
 
-        n_competidores = Cadastrar_Competidor(n_competidores);
+   printf("Informe o peso do competidor:");
+   scanf("%f", &competidor.peso);
 
-        printf("O numero de competidores é; %d", n_competidores);
-        
-            break;
+   // setbuf(stdin, NULL);
 
-        case 2:
+   printf("Informe a idade do competidor: ");
+   scanf("%d", &competidor.idade);
 
+   nCompetidores++;
 
-            break;
+   //  competidor.inscricao = rand()%99999; //Fazer inscrição sequencial baseado no numero de participantes + 1;
+   // Nova função cria o número de inscrição do competidor baseado na quantidade de competidores já inscritos, o número da inscrição será número de competidores + 1.
+   competidor.inscricao = nCompetidores;
 
-        case 3:
+   fprintf(cadastro, "\n%d %s %s %.2f %d", competidor.inscricao, competidor.nome, competidor.faixa, competidor.peso, competidor.idade);
 
+   fclose(cadastro);
 
-            break;
+   return nCompetidores;
+}
 
-        case 4:
+int main()
+{
+   int opc, nCompetidores = contaCompetidores();
 
+   competidor competidor;
 
-            break;
+   while (1)
+   {
+      printf("\n\n------- MENU -------- \n");
+      printf("1) CADASTRAR COMPETIDOR \n");
+      printf("2) REMOVER COMPETIDOR \n");
+      printf("3) ALTERAR DADOS DO COMPETIDOR \n");
+      printf("4) PESQUISAR COMPETIDORES\n");
+      printf("5) COMECAR CAMPEONATO \n");
+      printf("6) SAIR \n");
 
-        case 5:
-            
-            return 0;
+      printf("\n Digite a Opcao: ");
+      scanf("%d", &opc);
 
-            break;
+      setbuf(stdin, NULL);
 
-        default:
-            printf(" \n Opcao invalida! \n");
-            break;
-        }
-    }
+      switch (opc)
+      {
+      case 1:
+
+         nCompetidores = cadastrarCompetidor(nCompetidores);
+
+         printf("O numero de competidores eh: %d", contaCompetidores()); // Fazer os acentos funcionarem ??
+
+         break;
+
+      case 2:
+
+         break;
+
+      case 3:
+
+         break;
+
+      case 4:
+         //   Por enquanto, mostra apenas o número de competidores
+         printf("Numero de competidores: %d", nCompetidores);
+         achaCompetidor();
+
+         break;
+
+      case 5:
+         break;
+
+      case 6:
+
+         printf("Programa encerrado.");
+         return 0;
+         break;
+
+      default:
+         printf(" \n Opcao invalida! \n");
+         break;
+      }
+   }
 }
